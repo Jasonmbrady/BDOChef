cfd6f76c693e68eda08a924595d4a6a783c920f2
Jason M Brady <jasonmbrady42@gmail.com>
2020-12-21 05:53:21 -0800

    Update settings.py

--- BDOdb/settings.py
+++ BDOdb/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1$+36^7g^pi9yl+(5plra8-$zpwq_91vf=$0q^jwf0m$7@l+xk'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
